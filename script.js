6cd6ddaffb8083b9861464237469ba49e0f2095a
Stella Kim <62909702+nakim97@users.noreply.github.com>
2022-05-09 21:12:38 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const query = document.getElementById("promptInput");
+const responseContainer = document.getElementById("responseContainer");
+
+document.getElementById("submitBtn").addEventListener("click", function(e){
+    e.preventDefault();
+    fetchPrompt();
+})
+
+function fetchPrompt(){
+    const data = {
+        prompt: query.value,
+        temperature: 0.5,
+        max_tokens: 64,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+    };
+    
+    fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer 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`
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        return displayResponse(data);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+}
+
+function displayResponse(data){
+    responseContainer.innerHTML += `
+    <div id="responseCard">
+      <h3 id = "title"> Prompt: ${query.value}</h3>
+      <h3 id = "responseResult"> Response: ${data.choices[0].text}</h3>
+    </div>
+   `
+};
+
